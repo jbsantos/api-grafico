6983545a1f8b26edd8334fdc0f671e203fb92f55
jbsantos <jorge0302@gmail.com>
2021-09-30 17:25:16 -0300

    teste1.8

--- treinamento.py
+++ treinamento.py
@@ -32,7 +32,7 @@ def salvaDados():
     data = []
     #r = requests.get("https://apto-api-rest-ifpe.herokuapp.com/api/desafio-tecnico/rankearCandidatosSimplificado").json()
     #r = requests.get(" https://run.mocky.io/v3/3b892b3c-3ca8-42db-92d9-b2c164064c61").json()
-    r = requests.get("url = "https://run.mocky.io/v3/04af7d4f-67d9-44b2-880d-7f9158207564"").json()
+    r = requests.get("url = "https://run.mocky.io/v3/61703339-173a-4f8d-b235-edfe2405242e").json()
     notas = r['data']
     print(notas)
     print("notas")
