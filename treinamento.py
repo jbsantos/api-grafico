2fc0082468d3303699f9466650f7f0d70c147f8a
jbsantos <jorge0302@gmail.com>
2021-09-30 12:30:11 -0300

    treinamento versao antiga

--- treinamento.py
+++ treinamento.py
@@ -1,85 +1,36 @@
+import pandas as pd
+import numpy as np
 from sklearn.cluster import KMeans
-
-def objGrafico():
-    import numpy as np
-    import json
-
-    class NumpyEncoder(json.JSONEncoder):
-        def default(self, obj):
-            if isinstance(obj, np.ndarray):
-                return obj.tolist()
-            return json.JSONEncoder.default(self, obj)
-
-    #url = "https://apto-api-rest-ifpe.herokuapp.com/api/desafio-tecnico/rankearCandidatosSimplificado"
-    url = "https://run.mocky.io/v3/3b892b3c-3ca8-42db-92d9-b2c164064c61"
-    
-    infoNotas = salvaDados()
-    a = np.array(kmeansLabel())
-    img_json = np.array(jsonImagem())
-    json_dump = json.dumps({'legenda': a, 'imagem': img_json, 'infoNotas': infoNotas, 'url': url}, cls=NumpyEncoder)
-
-    return json_dump
-
-def salvaDados():
-
-    import numpy as np
-    import requests
-    import pandas as pd
-    import json
-    import requests
-    import re
-
-    data = []
-    #r = requests.get("https://apto-api-rest-ifpe.herokuapp.com/api/desafio-tecnico/rankearCandidatosSimplificado").json()
-    r = requests.get(" https://run.mocky.io/v3/3b892b3c-3ca8-42db-92d9-b2c164064c61").json()
-    #notas = r['data'][0]['candidatoNotasDtoList']
-    
-    for idx, val in enumerate(r['data']):
-        #data = val['candidatoNotasDtoList']
-        data.append(val['candidatoNotasDtoList'][0])
-     
-    dataFrame = pd.DataFrame(data)
-    #print(dataFrame)
-    df = pd.DataFrame(dataFrame)
-    print(df)
-    df.to_csv(index=False)
-    df.to_csv('aptoClassificacao/Apto_KNN.csv', index=False, encoding='utf-8')  
-    return r
- 
-
-def kmeansLabel():
-
-    import numpy as np
-    import pickle
-    import pandas as pd
-    apto = pd.read_csv('aptoClassificacao/Apto_KNN.csv')
-    X = apto.iloc[:, 2:4].values
-
-    kmeans = KMeans(n_clusters=2, init='random')
-    distancias = kmeans.fit_transform(X)
-    legendas = kmeans.labels_
-
-    with  open("kmeans_n_cluster4_padrao.pkl", "wb") as file: pickle.dump(kmeans, file)
-
-    import matplotlib.pyplot as plt
-    plt.scatter(X[:, 0], X[:,1], s= 100 , c = kmeans.labels_)
-    plt.scatter(kmeans.cluster_centers_[:,0], kmeans.cluster_centers_[:, 1],  s=100, c= 'blue', label = 'Centroids')
-    plt.title('APTO Clusters Questionário vs Programação')
-    plt.xlabel('Questionario')
-    plt.ylabel('Programacao')
-    plt.legend()
-    plt.savefig("static/grafico.jpeg")
-    plt.close()
-    return legendas
+import pickle
 
 
-def jsonImagem():
+apto = pd.read_csv('/home/jorge/Documentos/python/api/aptoClassificacao/Apto_KNN.csv')
+#apto = pd.read_csv('Apto_KNN normalizado.csv')
+X = apto.iloc[:, 1:5].values
+# print(X)
+# kmeans = KMeans(n_clusters=4, init='ndarray[[2,2],[2,8],[8,2],[8,8]]')
+kmeans = KMeans(n_clusters=4, init='random')
+distancias = kmeans.fit_transform(X)
+# print(distancias)
+print(legendas)
 
-    import base64
-    data = {}
+def teste():
 
-    with open('static/grafico.jpeg', mode='rb') as file:
-        img = file.read()
-    data['img'] = base64.encodebytes(img).decode('utf-8').replace('\n', '')
 
-    return data
+# print ("\n======\n", kmeans.cluster_centers_, "\n======\n")
+with  open("kmeans_n_cluster4_padrao.pkl", "wb") as file: pickle.dump(kmeans, file)
+legendas = kmeans.labels_
+import matplotlib.pyplot as plt
+plt.scatter(X[:, 0], X[:,1], s= 50 , c = kmeans.labels_)
+plt.scatter(kmeans.cluster_centers_[:,0], kmeans.cluster_centers_[:, 1],  s=300, c= 'red', label = 'Centroids')
+plt.title('APTO Clusters Front vs Back')
+plt.xlabel('FrontEnd')
+plt.ylabel('BackEnd')
+plt.legend()
+plt.savefig("/home/jorge/Documentos/python/api/static/grafico.jpeg")
+
+plt.show()
+
+#xy = new xy(X[0], X[1])
+
+#return {x=X[:, 0], y=X[:,1], labels=kmeans.labels_}
