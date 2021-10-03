e10e5328892241089e5c98f2568b64974e189df2
jbsantos <jorge0302@gmail.com>
2021-10-03 18:04:47 -0300

    update grafico 1.4

--- treinamento.py
+++ treinamento.py
@@ -34,17 +34,13 @@ def salvaDados():
     #r = requests.get("https://run.mocky.io/v3/bd659a0b-5b5f-4989-b47d-657076841398").json()
     #r = requests.get("https://run.mocky.io/v3/61703339-173a-4f8d-b235-edfe2405242e").json()
    
-    notas = []
+notas = []
 notas = r['data']
 data = []
-    #print(notas)
-    #print("notas")
-    #print(notas[0]['candidatoNotasDtoList'])   
-        
+
     for idx, val in enumerate(notas):
-        #for count, nota in enumerate(val['candidatoNotasDtoList']):
-        data.append(val)
-        print(val)
+        data.append(val['candidatoNotasDtoList'])
+        print(data)
    
     dataFrame = pd.DataFrame(data)
     df = pd.DataFrame(dataFrame)
